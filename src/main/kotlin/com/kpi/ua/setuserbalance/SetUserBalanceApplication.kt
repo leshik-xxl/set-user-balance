447dd9c4fd270e22713097fd62b628b01d1efb16
obikerei <oleksii.bikerei@intellias.com>
2024-05-27 19:29:18 +0300

    Initial commit

---
+++ src/main/kotlin/com/kpi/ua/setuserbalance/SetUserBalanceApplication.kt
@@ -0,0 +1,11 @@
+package com.kpi.ua.setuserbalance
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SetUserBalanceApplication
+
+fun main(args: Array<String>) {
+    runApplication<SetUserBalanceApplication>(*args)
+}
---
+++ src/test/kotlin/com/kpi/ua/setuserbalance/SetUserBalanceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kpi.ua.setuserbalance
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SetUserBalanceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
